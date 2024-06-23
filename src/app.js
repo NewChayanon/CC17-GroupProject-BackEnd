03c4878e9aceb52174b53d47942f489b015e0fa4
Sirawit44 <164641741+Sirawit44@users.noreply.github.com>
2024-06-23 21:33:43 +0700

    feature/errorMiddleware

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const notFound = require('./middlewares/not-found');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express()
 
 
 app.use(notFound)
+app.use(errorMiddleware)
 
 port = process.env.PORT
 app.listen(port, ()=> console.log('server running on', port))

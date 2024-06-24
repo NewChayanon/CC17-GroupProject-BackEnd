da3e68ef85896207cbd8bc034f2f8bfded6356c6
newchayanon <new.chayanon20054@gmail.com>
2024-06-24 15:51:23 +0700

    edit/api/fetchEvent

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const userController = require('../controllers/user-controller')
 const userRouter = express.Router()
 
-userRouter.get('/event')
+userRouter.get('/event',userController.findEventListOfUser)
 
 module.exports = userRouter

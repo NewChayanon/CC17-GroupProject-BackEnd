8e06d101693f91edc107ccb52d6e31f552473815
Sirawit44 <164641741+Sirawit44@users.noreply.github.com>
2024-06-24 17:57:56 +0700

    create/validator

---
+++ src/validators/auth-validator.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+
+exports.registerSchema = Joi.object({
+  firstName : Joi.string().required().trim(),
+  lastName: Joi.string().required().trim(),
+  email : Joi.string().email({tlds: false}).required().strip(),
+  password : Joi.string().required().pattern(/^[a-zA-Z0-9!@#$%^&*()-_=+?]{6,12}$/),
+  confirmPassword : Joi.string().required().valid(Joi.ref('password')).strip(),
+  mobile : Joi.string().pattern(/^[0-9]{10}$/),
+  role: Joi.string()
+});
+
+exports.loginSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().required()
+});

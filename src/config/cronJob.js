bd944ee7b638a24acc27c5152b2c92c73263f4a3
newchayanon <new.chayanon20054@gmail.com>
2024-07-11 18:08:37 +0700

    edit code as aribnb starndard

--- src/config/cronJob.js
+++ src/config/cronJob.js
@@ -14,23 +14,26 @@ cron.schedule("* * * * *", async () => {
     if (eventIds.length > 0) {
       await eventServices.updateManyIsActiveIsTrueInId(eventIds);
     }
+
     const allEventIsActive = await eventServices.findManyEventIdAndByDateAndIsActiveIsTrue(dateToday);
 
-    for (let el of allEventIsActive) {
+    for (const el of allEventIsActive) {
       const senderId = el.storeProfile.userId;
-      const userInterestId = el.Interest.map((el) => el.userId);
-      const userFollowId = el.storeProfile.Follow.map((el) => el.userId);
-      const userVoucherItemId = el.VoucherList.flatMap((el) => el.VoucherItem.map((item) => item.userId));
-      const userId = [...new Set([...userInterestId, ...userFollowId, ...userVoucherItemId])];
+      const userInterestIds = el.Interest.map((interest) => interest.userId);
+      const userFollowIds = el.storeProfile.Follow.map((follow) => follow.userId);
+      const userVoucherItemIds = el.VoucherList.flatMap((voucher) => voucher.VoucherItem.map((item) => item.userId));
+      const userIds = [...new Set([...userInterestIds, ...userFollowIds, ...userVoucherItemIds])];
 
-      const data = userId.map((element) => ({
+      const messages = userIds.map((userId) => ({
         userIdSender: senderId,
-        userIdReceiver: element,
-        topic: `event ${el.id} notification`,
-        message: `Hi Natcha16! The event of your interest and the store "Durian Lovelove" that you followed : "Durian 7.7. Super Promo" will be taken place tomorrow on 14 July 2024 at K-Village Rama IV from 10:00 - 18:00. See you soon! :)
-      Durian Lovelove`,
+        userIdReceiver: userId,
+        topic: `Event ${el.id} Notification`,
+        message: `Hi Natcha16! The event of your interest and the store "Durian Lovelove" that you followed: "Durian 7.7. Super Promo" will take place tomorrow on 14 July 2024 at K-Village Rama IV from 10:00 - 18:00. See you soon! :) Durian Lovelove`,
       }));
-      await inboxMessageUserService.createManyMessageByData(data);
+
+      if (messages.length > 0) {
+        await inboxMessageUserService.createManyMessageByData(messages);
+      }
     }
   } catch (err) {
     console.error("Error updating events:", err);

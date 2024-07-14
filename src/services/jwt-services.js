8c0d8ae2339f7a8933b9383f294b9468d58588dc
newchayanon <new.chayanon20054@gmail.com>
2024-07-14 16:08:46 +0700

    add res key eventDescription, voucherDescription, eventLocationName

--- src/services/jwt-services.js
+++ src/services/jwt-services.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const jwtService = {};
 
-jwtService.sign = (payload) => jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+jwtService.sign = (payload) => jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" });
 
 jwtService.verify = (token) => jwt.verify(token, process.env.JWT_SECRET);
 module.exports = jwtService;
